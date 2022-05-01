cb5215b38a356549c638ae4080eefec2ee8e71e1
selenabarrameda <98042521+selenabarrameda@users.noreply.github.com>
2022-05-01 23:23:04 +0800

    Update lab4.c

--- lab 4 (by pair)/lab4.c
+++ lab 4 (by pair)/lab4.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 
-int input_num[1000];
 int main(void){
-  int elements = 0;
-  int k = 22;
+  int num;
+  char decision;
+  
+  printf("Enter number: ");    // ?? tama ba na number ?? hehe pero pwede pa rin naman'g ma-modify 'to hehe
+  scanf("%d", &num);
+         
+  do(                          // do-while 'yong mairerecom ko para sa program na 'to hehe sksk pero kung may mas better, open ako for changes hehe uwu
+    printf("Enter digits: ");  // saasdmaksm digits ba ?? HUHU
+    scanf(
+      
+    // arranging the digits to ascending order
+      
+    // finding the index of the value inserted by user
+      
+    )
+    while(decision = "y");
 }
