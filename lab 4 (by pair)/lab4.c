a3ff9e6f27ab0933faeb060af325eb96f679d96e
selenabarrameda <98042521+selenabarrameda@users.noreply.github.com>
2022-05-03 01:19:47 +0800

    Update lab4.c

--- lab 4 (by pair)/lab4.c
+++ lab 4 (by pair)/lab4.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 
-int main(void){
-  int num;
-  char decision;
-  
-  printf("Enter number: ");    // ?? tama ba na number ?? hehe pero pwede pa rin naman'g ma-modify 'to hehe
-  scanf("%d", &num);
-         
-  do(                          // do-while 'yong mairerecom ko para sa program na 'to hehe sksk pero kung may mas better, open ako for changes hehe uwu
-    printf("Enter digits: ");  // saasdmaksm digits ba ?? HUHU
-    scanf(
-      
-    // arranging the digits to ascending order
-      
-    // finding the index of the value inserted by user
-      
-    )
-    while(decision = "y");
+	int len, i, list[len], number_to_check;
+	char decision;
+	
+	do{                              // hehe recom ko na do-while yung gagamitin dito hehe. pero pwede pa namang magbago if may mas better hehe
+		len = 0;
+		
+		printf("Enter a number: ");
+		scanf("%d", &len);
+		
+		for(i = 1; i <= len; i++){     // isa-isa pag-s-save ng digits?? 
+			printf("Number %d: ", i);
+			scanf("%d", &list[i]);
+		}
+		
+    // --------------------  ascending part ----------------------
+    
+    
+		printf("Which of the following values would you like to know their position (in an organized manner)?:");
+		scanf("%d", &number_to_check);
+		
+		i = 0;
+		
+		while(number_to_check != list[i]){
+			i++;
+		}
+		
+		printf("The position of the value inserted is #%d \n", i);
+		printf("Would you like to continue using the program? \"y\" for yes and \"n\" for no: ");
+		scanf(" %c", &decision);
+		
+		if (decision != 'y'){
+			printf("Thank you for using this program!");
+		}
+	}
+	while(decision == 'y');
+	
 }
