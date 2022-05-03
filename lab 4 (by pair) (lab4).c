c5eb4a71adf80632d7d3dc398234163e9565cea0
Gliezel Ann Pajarilla <93700998+Geez-l@users.noreply.github.com>
2022-05-03 19:07:05 +0700

    Update lab 4 (by pair) (lab4).c

--- lab 4 (by pair) (lab4).c
+++ lab 4 (by pair) (lab4).c
@@ -1,4 +1,23 @@
 #include <stdio.h>
 
-int linearSearch;
-int selection 
+int inputMax[1000];
+
+int main(void){
+  
+  int selection = 0, i,max;
+  int linearSearch, i, j;
+  int terminate = 1;
+  
+  printf("\n\t\t Greetings, dear user!");
+  printf("\n This program is about sorting and searching using: \n Selection Sort and Linear Search.\n";
+         
+  printf("Enter the number of elements in the array: ");
+  scanf("%d", &max);
+  int array[max];
+         
+  printf("Enter positive integers: \n);
+         
+  for (i = 0; i < n ; i++)
+  scanf("%d", array[i];
+         
+         
