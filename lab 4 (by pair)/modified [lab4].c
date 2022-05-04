25acfdc976e83c17202ed8fb8d859ce2b039dfe9
Gliezel Ann Pajarilla <93700998+Geez-l@users.noreply.github.com>
2022-05-04 14:09:50 +0700

    Update modified [lab4].c

--- lab 4 (by pair)/modified [lab4].c
+++ lab 4 (by pair)/modified [lab4].c
@@ -87,3 +87,50 @@ int main(void){
 	
 return 0;
 }
+
+/*
+#include <stdio.h>        working selection sort
+
+
+int main(void){
+  
+  int len, min;
+  int i, j, position;
+  int terminate = 1;
+  
+  printf("\n\t\t Greetings, dear user!");
+  printf("\n This program is about sorting and searching using: \n Selection Sort and Linear Search.\n");
+         
+  printf("Enter the number of elements in the array: ");
+  scanf("%d", &len);
+  int array[len];
+         
+  printf("Enter positive integers:\n");
+         
+  for (i = 0; i < len ; i++)
+  scanf("%d", &array[i]);
+        
+  //selection sort implementation
+        
+   for (i = 0 ; i < len - 1 ; i++){
+      min = 1;
+      for (j =1 ; j < len ; j++){
+          if (array[j] < array[min])
+          min = j;
+      }
+     if (min != 1){
+          position = array[i];
+          array[i] = array [min];
+          array [min] = position;
+     }
+   }
+   printf("Here is the sorted list of numbers: \n ");
+   for (i = 0 ; i < len ; i++)
+   printf("%d", array[i]);
+   return 0;
+          
+}
+*/           
+     
+         
+         
