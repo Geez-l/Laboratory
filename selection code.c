587e3dc7f602f53657ef9ca9320d09fb18bd1278
Gliezel Ann Pajarilla <93700998+Geez-l@users.noreply.github.com>
2022-05-04 18:32:37 +0700

    Update selection code.c

--- selection code.c
+++ selection code.c
@@ -1,47 +1,47 @@
 #include <stdio.h>
 
-int inputMax[1000];
 
 int main(void){
   
-  int selection = 0, i,max, arr, min;
-  int linearSearch, j;
+  int len, min;
+  int i, j, position;
   int terminate = 1;
   
+  printf("--------------------------------------------------------------------------------------------");
   printf("\n\t\t Greetings, dear user!");
   printf("\n This program is about sorting and searching using: \n Selection Sort and Linear Search.\n");
+  printf("---------------------------------------------------------------------------------------------");
          
   printf("Enter the number of elements in the array: ");
-  scanf("%d", &max);
-  int array[max];
-         
-  printf("Enter positive integers:\n");
-         
-  for (i = 0; i < max ; i++)
-  scanf("%d", &array[i]);
+  scanf("%d", &len);
+  int array[len];
+  
+  if (len <1){
+    printf("Invalid input. Please try again!");
+    break;
+  }
+  else{
+    printf("Enter elements in the array:\n");
+    for (i = 0; i < len ; i++)
+    printf(
+    scanf("%d", &array[i]);
         
   //selection sort implementation
         
-   for (i = 0 ; i < max - 1 ; i++){
+   for (i = 0 ; i < len - 1 ; i++){
       min = 1;
-      for (j =1 ; j < max ; j++){
+      for (j =1 ; j < len ; j++){
           if (array[j] < array[min])
           min = j;
       }
      if (min != 1){
-          arr = array[i];
+          position = array[i];
           array[i] = array [min];
-          array [min] = arr;
+          array [min] = position;
      }
    }
    printf("Here is the sorted list of numbers: \n ");
-   for (i = 0 ; i < max ; i++)
+   for (i = 0 ; i < len ; i++)
    printf("%d", array[i]);
    return 0;
-          
 }
-   
-          
-     
-         
-         
